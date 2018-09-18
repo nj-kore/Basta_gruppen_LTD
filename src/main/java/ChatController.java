99da5075f1ece5f3f3b5dec87305a148d793234a
hagerg <42995901+hagerg@users.noreply.github.com>
2018-09-18 16:04:18 +0200

    Connected fxml-tags to their respective controllers and changed so that Message.fxml has a root:fxml. This makes it so that several messages can be created.

--- src/main/java/ChatController.java
+++ src/main/java/ChatController.java
@@ -1,6 +1,9 @@
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
+
 
 import java.awt.*;
 
