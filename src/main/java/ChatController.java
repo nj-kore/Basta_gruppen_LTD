478ee2f4b240ee50bf4b0d70b9f5225e6ab16e65
Vinnerholt <benno_fotboll@hotmail.com>
2018-09-18 12:03:52 +0200

    ChatController och MainController

---
+++ src/main/java/ChatController.java
@@ -0,0 +1,17 @@
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+
+import java.awt.*;
+
+public class ChatController {
+
+    @FXML
+    FlowPane ChatFlowPane;
+
+    @FXML
+    Button SendButton;
+
+    @FXML
+    TextArea ChatTextArea;
+}
---
+++ src/main/java/MainController.java
@@ -0,0 +1,16 @@
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+
+public class MainController {
+
+    @FXML
+    AnchorPane MainViewAnchorPane;
+
+    @FXML
+    FlowPane ContactsFlowPane;
+
+    @FXML
+    FlowPane ConversationsFlowPane;
+
+}
