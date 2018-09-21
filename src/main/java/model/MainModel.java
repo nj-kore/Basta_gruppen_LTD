87eb7d74ed3f5771129db9dc01e8d57764d2125b
GustafSpjut <33220679+GustafSpjut@users.noreply.github.com>
2018-09-21 10:22:34 +0200

    Forgot update observable

--- src/main/java/model/MainModel.java
+++ src/main/java/model/MainModel.java
@@ -2,10 +2,16 @@ package model;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Observable;
 
 public class MainModel implements MainModelinterface, DataBaseHandlerinterface {
     private FileHandler fh = new FileHandler();
     private User activeUser = new User(0);
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
 
     public void sendMessage(int conversationId, Message message) {
         String text = message.getSender().getId()+";"+message.getText()+"\n";
