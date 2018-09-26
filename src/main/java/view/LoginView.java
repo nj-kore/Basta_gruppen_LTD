917360b0e1872d85888d369379bb9b6e930ebe85
Vinnerholt <benno_fotboll@hotmail.com>
2018-09-26 18:59:15 +0200

    Merge branch 'master' of C:\Users\benja\IdeaProjects\Basta_gruppen_LTD with conflicts.

---
+++ src/main/java/view/LoginView.java
@@ -0,0 +1,46 @@
+package view;
+
+import controller.ILoginController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import model.MainModel;
+
+import java.io.IOException;
+
+public class LoginView extends AnchorPane implements ILoginController {
+    @FXML
+    TextField userNameTextField;
+    @FXML
+    PasswordField passwordField;
+    @FXML
+    Button loginButton;
+
+    MainView parentView;
+
+    public LoginView(MainView parentView) {
+        this.parentView = parentView;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../../resources/fxml/LoginView.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        userNameTextField.requestFocus(); //Gives focus to userNameTextField
+
+    }
+
+    @FXML
+    public void login(){
+        if (MainModel.getInstance().login(userNameTextField.getText(), passwordField.getText())){
+            parentView.moveLoginToBack();
+        }
+    }
+}
