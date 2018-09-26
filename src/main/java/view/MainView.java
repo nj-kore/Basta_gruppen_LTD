cc0de28101ea036ed4f3550ab60827a7699ccaef
Filip Andréasson <filand1203@gmail.com>
2018-09-26 17:00:11 +0200

    minor fix

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -5,13 +5,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
-<<<<<<< HEAD
-=======
 import model.IMainModel;
 import model.MainModel;
 import model.User;
->>>>>>> f4b5bff3c7ef5b5bde1337c5fbda274068f5ac77
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
