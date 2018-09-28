48c7491708933e082b9e4f7f86e4638abfe18a92
Jonathan Köre <korejonathan@gmail.com>
2018-09-28 10:58:29 +0200

    A test for travis

--- src/main/java/model/MainModel.java
+++ src/main/java/model/MainModel.java
@@ -31,7 +31,8 @@ public class MainModel implements IMainModel {
             dataHandler.updateConversation(c);
         } else {
             c = dataHandler.loadConversation(conversationId);
-            addConversation(c);
+            if(c != null)
+                addConversation(c);
         }
         return c;
     }
