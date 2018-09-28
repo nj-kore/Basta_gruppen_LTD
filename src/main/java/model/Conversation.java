182b3a32e51652671146e03204e31e0957ca51cd
hagerg <42995901+hagerg@users.noreply.github.com>
2018-09-28 10:25:09 +0200

    hopefully fixed travis error by changing arraylist to list.

--- src/main/java/model/Conversation.java
+++ src/main/java/model/Conversation.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public class Conversation {
     private int id;
-    private ArrayList<Message> messages = new ArrayList();
-    private ArrayList<User> participants = new ArrayList();
+    private List<Message> messages = new ArrayList();
+    private List<User> participants = new ArrayList();
     private String name;
 
     public String getName() {
@@ -38,7 +38,7 @@ public class Conversation {
         participants.add(u);
     }
 
-    public ArrayList<Message> getMessages() {
+    public List<Message> getMessages() {
         return messages;
     }
 }
