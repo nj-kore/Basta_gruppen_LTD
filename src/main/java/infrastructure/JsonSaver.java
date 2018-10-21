663251b89fb5a57228d3b774368a6c2c3c62e31a
hagerg <42995901+hagerg@users.noreply.github.com>
2018-10-21 18:38:42 +0200

    Edited and added some javadoc

--- src/main/java/infrastructure/JsonSaver.java
+++ src/main/java/infrastructure/JsonSaver.java
@@ -1,7 +1,7 @@
 /**
  * A class that saves MainModel data to JSON.
  *
- * @author          Gustav Häger
+ * @author          gradGustav Häger
  * responsibility:  To save data to "database" (json files).
  * used by:         MainModel (as an observer)
  * used for:        Saving data to the "database" (json files).
